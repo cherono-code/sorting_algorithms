c454cb09aaf2131f24ee69e0efc677f4f8c6ae89
KIMO892004 <kimoragab2004@gmail.com>
2023-07-25 13:30:48 -0700

    k

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,70 +1,72 @@
 #include "sort.h"
 
 /**
- * quick_sort - sorts an array of integers in ascending order
- * @array: the array of integers
+ * partition - partitions an array
+ * @arr: the array
+ * @left: the leftmost index
+ * @right: the rightmost index
+ * @size: the full array size
+ * Return:the pivot index
+ */
+int partition(int *arr, int left, int right, size_t size)
+{
+	int i, k, pivot, tmp;
+
+	pivot = arr[right];
+	i = left;
+
+	for (k = left; k < right; k++)
+	{
+		if (arr[k] < pivot)
+		{
+			if (i != k)
+			{
+				tmp = arr[k];
+				arr[k] = arr[i];
+				arr[i] = tmp;
+				print_array(arr, size);
+			}
+			i++;
+		}
+	}
+	if (arr[i] != arr[right])
+	{
+		tmp = arr[i];
+		arr[i] = arr[right];
+		arr[right] = tmp;
+		print_array(arr, size);
+	}
+
+	return (i);
+}
+
+/**
+ * sorter - recursive sorting algorithm
+ * @arr: the array
+ * @left: the leftmost index
+ * @right: the rightmost index
+ * @size: the full size of array
+ */
+void sorter(int *arr, int left, int right, size_t size)
+{
+	int pivot;
+
+	if (left < right)
+	{
+		pivot = partition(arr, left, right, size);
+		sorter(arr, left, pivot - 1, size);
+		sorter(arr, pivot + 1, right, size);
+	}
+}
+
+/**
+ * quick_sort - quicksort algorithm
+ * @array: the array to be sorted
  * @size: the size of array
  */
 void quick_sort(int *array, size_t size)
 {
-	partition(array, 0, size - 1, size);
-}
-
-/**
- * partition - the divide and conquer
- * @array: the list of integers
- * @bot: the first integer
- * @top: the last integer
- * @size: the length of array
- */
-void partition(int *array, int bot, int top, size_t size)
-{
-	if (bot < top)
-	{
-		int sorted = sorted_array(array, bot, top, size);
-
-		partition(array, bot, sorted - 1, size);
-		partition(array, sorted + 1, top, size);
-	}
-}
-
-/**
- * sorted_array - the divide and conquer
- * @array: the list of integers
- * @bot: the first integer
- * @top: the last integer
- * @size: the length of array
- * Return:- Always 0
- */
-
-int sorted_array(int *array, int bot, int top, size_t size)
-{
-	int swap, new_pivot;
-	int pivot = array[top];
-	int k = bot, j = bot;
-
-	for (; j < top; j++)
-	{
-		if (array[j] <= pivot)
-		{
-			if (k != j)
-			{
-				swap = array[k];
-				array[k] = array[j];
-				array[j] = swap;
-				print_array(array, size);
-			}
-			k++;
-		}
-	}
-
-	if (pivot != array[k])
-	{
-	new_pivot = array[k];
-	array[k] = array[top];
-	array[top] = new_pivot;
-	print_array(array, size);
-	}
-
-	return (k);
+	if (array == NULL || size <= 1)
+		return;
+	sorter(array, 0, size - 1, size);
 }
