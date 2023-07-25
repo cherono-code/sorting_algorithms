b64120f9ddf6744a60ea528c62de96d2b013dcbd
KIMO892004 <kimoragab2004@gmail.com>
2023-07-25 13:59:50 -0700

    k

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -10,11 +10,11 @@
 
 void swap_nodes(listint_t *node1, listint_t *node2, listint_t **head)
 {
-	listint_t *tmp = node1->prev;
+	listint_t *tmpp = node1->prev;
 
-	if (tmp != NULL)
-		tmp->next = node2;
-	node2->prev = tmp;
+	if (tmpp != NULL)
+		tmpp->next = node2;
+	node2->prev = tmpp;
 	node1->prev = node2;
 	node1->next = node2->next;
 	node2->next = node1;
@@ -26,8 +26,8 @@ void swap_nodes(listint_t *node1, listint_t *node2, listint_t **head)
 
 /**
   * len_list - computes lenght of linked list
-  * @list: head node of list
-  * Return: lenght of the list
+  * @list: the head node of list
+  * Return: the lenght of the list
   */
 
 int len_list(listint_t *list)
@@ -44,32 +44,32 @@ int len_list(listint_t *list)
 
 /**
  * cocktail_sort_list - cocktail sorting algo
- * @list: pointer to list to sort
+ * @list: the pointer to list to sort
  * Return: void
  */
 
 void cocktail_sort_list(listint_t **list)
 {
 	int start = 0, end, flag = 1, counter = 0;
-	listint_t *tmp;
+	listint_t *tmpp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	tmp = *list;
+	tmpp = *list;
 	end = len_list(*list);
 	while (flag)
 	{
 		flag = 0;
 		while (counter < end - 1)
 		{
-			if (tmp->n > tmp->next->n)
+			if (tmpp->n > tmpp->next->n)
 			{
 				flag = 1;
-				swap_nodes(tmp, tmp->next, list);
+				swap_nodes(tmpp, tmpp->next, list);
 				print_list(*list);
 			}
 			else
-				tmp = tmp->next;
+				tmpp = tmpp->next;
 			counter++;
 		}
 		end--;
@@ -78,14 +78,14 @@ void cocktail_sort_list(listint_t **list)
 		flag = 0;
 		while (counter > start)
 		{
-			if (tmp->n < tmp->prev->n)
+			if (tmpp->n < tmpp->prev->n)
 			{
 				flag = 1;
-				swap_nodes(tmp->prev, tmp, list);
+				swap_nodes(tmpp->prev, tmpp, list);
 				print_list(*list);
 			}
 			else
-				tmp = tmp->prev;
+				tmpp = tmpp->prev;
 			counter--;
 		}
 		start++;
